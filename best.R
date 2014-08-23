83bd50614d1e7badb2c71c5b58c8f3d3d2d6ad78
jamespbrink <james.p.brink@gmail.com>
2014-08-23 08:20:18 -0400

    best started

---
+++ best.R
@@ -0,0 +1,11 @@
+best <- function(state, outcome){
+        ## read outcome data
+        
+        ## check that state and outcome are valid
+        ## throw error and stop and print "invalid state" if state is invalid
+        ## throw error and stop and print "invalid outcome" if outcome is invalid
+        
+        ##return hospital name in that state with lowest 30 day death rate
+        ##tie goes to the first hospital alphabetically
+        
+}
