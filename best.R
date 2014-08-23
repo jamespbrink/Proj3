faa9ca1ba081a8e8a25ec3c524049bc19e3f5df5
jamespbrink <james.p.brink@gmail.com>
2014-08-23 08:46:36 -0400

    started best

--- best.R
+++ best.R
@@ -1,9 +1,11 @@
 best <- function(state, outcome){
         ## read outcome data
+        data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         
         ## check that state and outcome are valid
         ## throw error and stop and print "invalid state" if state is invalid
         ## throw error and stop and print "invalid outcome" if outcome is invalid
+        sum(state == data[,7])
         
         ##return hospital name in that state with lowest 30 day death rate
         ##tie goes to the first hospital alphabetically
